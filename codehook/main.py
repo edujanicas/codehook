df635b71a1f9898c7b8aaece4f664fbbdeeb1c5b
Eduardo Janicas <edujanicas@icloud.com>
2023-09-08 16:58:00 +0100

    Refactor deployer into separate class

--- codehook/main.py
+++ codehook/main.py
@@ -3,18 +3,7 @@ import logging
 
 from rich import print
 from dotenv import load_dotenv
-from .RestBasics import RestWrapper
-from .LambdaBasics import LambdaWrapper
-from .retries import wait
-
-import os.path
-import calendar
-import datetime
-import json
-import logging
-import time
-import boto3
-import requests
+from .RestDeployer import RestDeployer
 
 
 logger = logging.getLogger(__name__)
@@ -29,109 +18,6 @@ def callback():
     """
 
 @app.command()
-def deploy(basic_file):
-    """
-    Shows how to deploy an AWS Lambda function, create a REST API, call the REST API
-    in various ways, and remove all of the resources after the demo completes.
-    """
-
-    logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
-    print("-" * 88)
-    print("Welcome to the AWS Lambda and Amazon API Gateway REST API creation demo.")
-    print("-" * 88)
-
-    lambda_filename = basic_file
-    lambda_handler_name = "lambda_handler_rest.lambda_handler"
-    lambda_role_name = "demo-lambda-role"
-    lambda_function_name = "demo-lambda-rest"
-    api_name = "demo-lambda-rest-api"
-
-    iam_resource = boto3.resource("iam")
-    lambda_client = boto3.client("lambda")
-    apigateway_client = boto3.client("apigateway")
-    
-    lambda_wrapper = LambdaWrapper(lambda_client, iam_resource)
-    rest_wrapper = RestWrapper(apigateway_client)
-
-    print("Checking for IAM role for Lambda...")
-    iam_role, should_wait = lambda_wrapper.create_iam_role_for_lambda(lambda_role_name)
-    if should_wait:
-        logger.info("Giving AWS time to create resources...")
-        wait(10)
-
-    print(
-        f"Creating AWS Lambda function {lambda_function_name} from "
-        f"{lambda_handler_name}..."
-    )
-    # destination file without the full path
-    deployment_package = lambda_wrapper.create_deployment_package(
-        lambda_filename, os.path.basename(lambda_filename)
-    )
-    lambda_function_arn = lambda_wrapper.create_function(
-        lambda_function_name, lambda_handler_name, iam_role, deployment_package
-    )
-
-    print(f"Creating Amazon API Gateway REST API {api_name}...")
-    account_id = boto3.client("sts").get_caller_identity()["Account"]
-    api_base_path = "demoapi"
-    api_stage = "test"
-    api_id = rest_wrapper.create_rest_api(
-        api_name,
-        api_base_path,
-        api_stage,
-        account_id,
-        lambda_client,
-        lambda_function_arn,
-    )
-    api_url = rest_wrapper.construct_api_url(
-        api_id, api_stage, api_base_path
-    )
-    print(f"REST API created, URL is :\n\t{api_url}")
-    print(f"Sleeping for a couple seconds to give AWS time to prepare...")
-    time.sleep(2)
-
-    print(f"Sending some requests to {api_url}...")
-    https_response = requests.get(api_url)
-    print(
-        f"REST API returned status {https_response.status_code}\n"
-        f"Message: {json.loads(https_response.text)['message']}"
-    )
-
-    https_response = requests.get(
-        api_url,
-        params={"name": "Martha"},
-        headers={"day": calendar.day_name[datetime.date.today().weekday()]},
-    )
-    print(
-        f"REST API returned status {https_response.status_code}\n"
-        f"Message: {json.loads(https_response.text)['message']}"
-    )
-
-    https_response = requests.post(
-        api_url,
-        params={"name": "Martha"},
-        headers={"day": calendar.day_name[datetime.date.today().weekday()]},
-        json={"adjective": "fabulous"},
-    )
-    print(
-        f"REST API returned status {https_response.status_code}\n"
-        f"Message: {json.loads(https_response.text)['message']}"
-    )
-
-    https_response = requests.delete(api_url, params={"name": "Martha"})
-    print(
-        f"REST API returned status {https_response.status_code}\n"
-        f"Message: {json.loads(https_response.text)['message']}"
-    )
-
-    print("Deleting the REST API, AWS Lambda function, and security role...")
-    time.sleep(5)  # Short sleep avoids TooManyRequestsException.
-    lambda_wrapper.delete_function(lambda_function_name)
-    for pol in iam_role.attached_policies.all():
-        pol.detach_role(RoleName=iam_role.name)
-    iam_role.delete()
-    print(f"Deleted role {iam_role.name}.")
-    rest_wrapper.delete_rest_api(api_id)
-    print("Thanks for watching!")
-
-    
+def deploy(basic_file, lambda_name, api_name):
+   rest_deployer = RestDeployer(basic_file, lambda_name, api_name)
+   rest_deployer.deploy()
