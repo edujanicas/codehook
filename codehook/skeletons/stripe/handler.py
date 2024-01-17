483cc9a215591c7025ae730e3b13e4fb7c8be821
Eduardo Janicas <edujanicas@icloud.com>
2024-01-17 14:43:38 +0000

    Handler contains skeleton if elif code to handle different event types

--- codehook/skeletons/stripe/handler.py
+++ codehook/skeletons/stripe/handler.py
@@ -1,3 +1,12 @@
 def handler_logic(body):
     print("Handler logic skeleton")
+    # if event.type == 'payment_intent.succeeded':
+    #     payment_intent = event.data.object # contains a stripe.PaymentIntent
+    #     print('PaymentIntent was successful!')
+    # elif event.type == 'payment_method.attached':
+    #     payment_method = event.data.object # contains a stripe.PaymentMethod
+    #     print('PaymentMethod was attached to a Customer!')
+    # # ... handle other event types
+    # else:
+    #     print('Unhandled event type {}'.format(event.type))
     return (500, "Handler logic skeleton")
