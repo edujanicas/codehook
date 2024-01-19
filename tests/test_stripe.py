c3bc258edcc03d80fb3e602e64c1e22f495685b8
Eduardo Janicas <edujanicas@icloud.com>
2024-01-19 16:40:20 +0000

    Test Stripe Integration

--- tests/test_stripe.py
+++ tests/test_stripe.py
@@ -1,1 +1,38 @@
+import os
+
 import pytest
+
+from codehook.sources.stripe import Stripe
+
+
+@pytest.fixture(scope="module")
+def my_stripe():
+    stripe_api_key = os.getenv("STRIPE_API_KEY")
+    return Stripe(stripe_api_key)
+
+
+class TestStripe:
+    def test_list_webhooks(self, my_stripe):
+        result = my_stripe.list_webhooks()
+        assert result == []
+
+    def test_create_webhooks(self, my_stripe):
+        
+        events = ['*']
+        url = "https://example.com/webhook"
+        
+        result = my_stripe.create_webhook(events, url)
+        assert result.__contains__("we_")
+
+        my_stripe.delete_webhook(result)
+
+    def test_delete_webhooks(self, my_stripe):
+        
+        events = ['*']
+        url = "https://example.com/webhook"
+        
+        result = my_stripe.create_webhook(events, url)
+        my_stripe.delete_webhook(result)
+        
+        result = my_stripe.list_webhooks()
+        assert result == []
