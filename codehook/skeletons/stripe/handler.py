45255e40a8c7aa38ae18c48ca97274f9b0692a46
Eduardo Janicas <edujanicas@icloud.com>
2024-01-21 16:50:12 +0000

    Document input as per #11

--- codehook/skeletons/stripe/handler.py
+++ codehook/skeletons/stripe/handler.py
@@ -1,5 +1,11 @@
 def handler_logic(body):
-    print("Handler logic skeleton")
+    """
+    Handles the logic around a Stripe webhook event
+    :param body: The event in JSON format. Example available on example_payload.json
+    :return: A tuple containing the HTTP status code and the body of the response.
+    The response body is used for logging purposes only, as webhooks are asynchronous.
+        (response_code, response_body)
+    """
     # if event.type == 'payment_intent.succeeded':
     #     payment_intent = event.data.object # contains a stripe.PaymentIntent
     #     print('PaymentIntent was successful!')
